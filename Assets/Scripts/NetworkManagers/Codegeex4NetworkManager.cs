231174145777cd35ea89fcf355a03f3454439916
Riccardo Caprile <riccardo.caprile97@gmail.com>
2024-11-12 09:57:34 +0100

    .

--- Assets/Scripts/NetworkManagers/Codegeex4NetworkManager.cs
+++ Assets/Scripts/NetworkManagers/Codegeex4NetworkManager.cs
@@ -1,18 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
+using System.Diagnostics;
+using UnityEngine.UI;
 
 public class Codegeex4NetworkManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string message;
+    private TcpClient client;
+    private NetworkStream stream;
+    [SerializeField] Button ServerButton;
+
+
+    //----------------------------- Python Scripts Location ---------------------------------------------------------------------------------------------
+
+    //Location must be checked in different devices
+
+    private string geexPy = "/k python C:\\Users\\ricky\\Desktop\\Framework\\Virtual-Reality-Customizable-Environments-\\PythonServer\\codegeex4.py"; //Laptop
+    //private string geexPy = "/k python C:\\Users\\ricky\\Desktop\\Virtual-Reality-Customizable-Environments-\\PythonServer\\codegeex4.py"; //Desktop
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------------
+    public void Start()
     {
-        
+
+
+        // Start receiving messages in a separate task
     }
 
-    // Update is called once per frame
-    void Update()
+    //Method that switch on the Gemini Python Server
+    public void QwenServerConnection()
     {
-        
+        Process.Start("cmd.exe", geexPy);
+        client = new TcpClient("127.0.1.3", 12340);
+        stream = client.GetStream();
+        ServerButton.interactable = false;
+
+    }
+
+    //It Contains the answered received from GEMINI
+    public async Task<string> ReceiveMessages()
+    {
+
+        byte[] buffer = new byte[102400];
+        int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+        if (bytesRead == 0)
+        {
+            // Handle disconnection
+            UnityEngine.Debug.Log("Disconnected from server!");
+            return "";
+        }
+
+        message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        return message;
+    }
+
+    public async Task SendMessageToServer(string message)
+    {
+
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        await stream.WriteAsync(data, 0, data.Length);
+
     }
 }
---
+++ PythonServer/codegeex4.py
@@ -0,0 +1,31 @@
+import ollama
+import socket
+import time
+HOST = '127.0.1.4'  # Standard loopback interface address (localhost)
+PORT = 12349        # Port to listen on (non-privileged ports are > 1023)
+model = "codegeex4"
+
+
+print("Welcome to the QWEN Python server for this Framework")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    s.listen()
+    conn, addr = s.accept()
+    print('Server in ascolto su', HOST, PORT)
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(102400)
+            print(data.decode())      
+            if(data.decode()!= "STOP"):
+                ollama_response = ollama.chat(
+                model = model,
+                messages = [{'role': 'user', 'content': data.decode()}],
+                
+                )
+                response = ollama_response['message']['content']
+                print(response)
+                     
+                conn.sendall(response.encode())   
+                time.sleep(5)  
