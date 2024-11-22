9a1cb9ef03da1957a4a38388d4d6aaa09dc45ef1
Riccardo Caprile <riccardo.caprile97@gmail.com>
2024-11-22 16:17:24 +0100

    Virtual Keyboard

---
+++ Assets/Scripts/Virtual Keyboard.cs
@@ -0,0 +1,41 @@
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VirtualKeyboard : MonoBehaviour
+{
+  
+    private TMP_InputField inputField;
+
+    public Transform positionSource;
+
+    public float distance = 0.5f;
+    public float verticalOffset = -0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        inputField.onSelect.AddListener(x => OpenKeyBoard());
+
+    }
+
+    public void OpenKeyBoard()
+    {
+        NonNativeKeyboard.Instance.InputField = inputField;
+        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+
+        Vector3 direction = positionSource.forward;
+
+        direction.y = 0;
+        direction.Normalize();
+
+        Vector3 targetPosition = positionSource.position + direction * distance + Vector3.up * verticalOffset;
+
+        NonNativeKeyboard.Instance.RepositionKeyboard(targetPosition);
+
+    }
+ 
+}
