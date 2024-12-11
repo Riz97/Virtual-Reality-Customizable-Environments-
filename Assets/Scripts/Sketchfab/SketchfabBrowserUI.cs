087934a36da807fd5880b5041d0c0bf775e67b7d
Riccardo Caprile <riccardo.caprile97@gmail.com>
2024-12-11 17:07:59 +0100

    .

--- Assets/Scripts/Sketchfab/SketchfabBrowserUI.cs
+++ Assets/Scripts/Sketchfab/SketchfabBrowserUI.cs
@@ -12,19 +12,24 @@ using System.Threading.Tasks;
 using System.Text;
 using UnityEditor;
 using System;
+using Debug = UnityEngine.Debug;
+using UnityEditor.PackageManager.Requests;
 
 public class SketchfabBrowser : MonoBehaviour
 {
     private TcpClient client;
     private NetworkStream stream;
     private string SketchfabPy = "/k python C:\\Users\\ricky\\Desktop\\Framework\\Virtual-Reality-Customizable-Environments-\\PythonServer\\SketchfabServer\\SketchfabDownloader.py";
-
+    string path = "C:\\Users\\ricky\\Desktop\\Framework\\Virtual-Reality-Customizable-Environments-\\Assets\\Imported";
+    public static string ImagePreview;
+    public bool preserveAspect = true; // Mantieni le proporzioni dell'immagine
 
     [Header("UI Elements")]
     public TMP_InputField keywordInput; 
     public Button searchButton;    
     public Transform contentParent; // Scrollview Content
     public GameObject modelItemPrefab; // Prefab , 2 Texts and 1 button
+    public TMP_Text UpdatingText;
 
     private string apiToken = "a2cba13cba97b522dfba8241b25334cf"; // Sostituisci con la tua API Key
     private string apiUrl = "https://api.sketchfab.com/v3/search?type=models";
@@ -64,38 +69,48 @@ public class SketchfabBrowser : MonoBehaviour
 
     private void DisplayModels(string jsonResponse)
     {
-        
+        // Pulisce la lista di modelli precedenti
         foreach (Transform child in contentParent)
         {
             Destroy(child.gameObject);
         }
 
-        //JSON Parsing
+        // Parsing del JSON
         JObject response = JObject.Parse(jsonResponse);
         JArray models = (JArray)response["results"];
 
         foreach (var model in models)
         {
-            //Extract the Model Name, author and URL
+            // Estrae il nome del modello, autore e URL
             string modelName = model["name"].ToString();
             string modelAuthor = model["user"]["username"].ToString();
             string modelUrl = model["viewerUrl"].ToString();
+            string imagePreview = model["thumbnails"]["images"][0]["url"].ToString();
 
-            //Creates a new prefab that represents one Sketchfab model record
+            // Aggiungi "https://" se manca nel link dell'immagine
+            if (!imagePreview.StartsWith("http://") && !imagePreview.StartsWith("https://"))
+            {
+                imagePreview = "https:" + imagePreview; // Aggiungi https:// al link se non presente
+            }
+
+            // Crea il prefab che rappresenta un record del modello Sketchfab
             GameObject modelItem = Instantiate(modelItemPrefab, contentParent);
 
-            //Find all the gameobjects needed
+            // Trova tutti i gameobject necessari
             TMP_Text nameText = modelItem.transform.Find("ModelName").GetComponent<TMP_Text>();
             TMP_Text authorText = modelItem.transform.Find("ModelAuthor").GetComponent<TMP_Text>();
             Button openButton = modelItem.transform.Find("OpenButton").GetComponent<Button>();
+            RawImage targetRawImage = modelItem.transform.Find("Preview").GetComponent<RawImage>();
 
-            //Assign the name taken from the JSON to the prefab
+            // Assegna il nome preso dal JSON al prefab
             nameText.text = modelName;
-            authorText.text =  modelAuthor;
-            
+            authorText.text = modelAuthor;
 
-            //Button behaviour if pressed
-            openButton.onClick.AddListener(() => OpenModelUrl(modelName.Replace(" ","")+" "+modelUrl));
+            // Scarica e applica l'immagine come texture
+            StartCoroutine(DownloadAndApplyImage(imagePreview, targetRawImage));
+
+            // Imposta il comportamento del bottone se premuto
+            openButton.onClick.AddListener(() => OpenModelUrl(modelName.Replace(" ", "") + " " + modelUrl));
         }
     }
 
@@ -114,17 +129,66 @@ public class SketchfabBrowser : MonoBehaviour
         stream = client.GetStream();
 
     }
-public void Update()
-{
+    IEnumerator DownloadAndApplyImage(string url,RawImage targetRawImage)
+    {
+        // Avvia il download dell'immagine
+        using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(url))
+        {
+            webRequest.disposeDownloadHandlerOnDispose = true;
+
+            // Aspetta il completamento della richiesta
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+            }
+            else
+            {
+                // Ottieni la texture
+                Texture2D downloadedTexture = DownloadHandlerTexture.GetContent(webRequest);
+
+                if (downloadedTexture != null)
+                {
+
+
+                    // Assegna la texture alla RawImage
+                    if (targetRawImage != null)
+                    {
+                        targetRawImage.texture = downloadedTexture;
+
+                        // Adatta l'immagine alla forma della RawImage
+                        if (preserveAspect)
+                        {
+                            AdjustAspectRatio(targetRawImage, downloadedTexture);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    void AdjustAspectRatio(RawImage rawImage, Texture2D texture)
+    {
+        // Ottieni il RectTransform della RawImage
+        RectTransform rectTransform = rawImage.GetComponent<RectTransform>();
+
+        // Calcola il rapporto tra larghezza e altezza
+        float textureAspectRatio = (float)texture.width / texture.height;
+        float rawImageAspectRatio = rectTransform.rect.width / rectTransform.rect.height;
+
+        if (textureAspectRatio > rawImageAspectRatio)
+        {
+            // L'immagine è più larga: ridimensiona l'altezza
+            rectTransform.sizeDelta = new Vector2(rectTransform.rect.width, rectTransform.rect.width / textureAspectRatio);
+        }
+        else
+        {
+            // L'immagine è più alta: ridimensiona la larghezza
+            rectTransform.sizeDelta = new Vector2(rectTransform.rect.height * textureAspectRatio, rectTransform.rect.height);
+        }
+    }
 
 }
-
-
-}
-
-
-
-
 
 #if UNITY_EDITOR
 
@@ -145,7 +209,6 @@ public static class PlayModeStateHandler
 
         if (state == PlayModeStateChange.ExitingPlayMode)
         {
-            UnityEngine.Debug.Log("Il PlayMode è stato fermato. Codice eseguito al termine della scena.");
           
             string folderPath = "C:\\Users\\ricky\\Desktop\\Framework\\Virtual-Reality-Customizable-Environments-\\Assets\\Imported";
 
@@ -155,9 +218,11 @@ public static class PlayModeStateHandler
             foreach (string zipFile in zipFiles)
             {
                 File.Delete(zipFile);
-                Console.WriteLine($"File zip eliminato: {zipFile}");
+                
             }
         }
     }
 }
 #endif
+
+
--- User Manual/SKETCHFAB README.txt
+++ User Manual/SKETCHFAB README.txt
@@ -1,7 +1,7 @@
 Path to get an Authorization Key
 
 1)Write to the Sketchfab Help center to provide you a Client Id and Secret Id
-2)Go to https://sketchfab.com/oauth2/token/"YOUR_CLIENT_ID"
+2)Go to "https://sketchfab.com/oauth2/authorize/?state=123456789&response_type=token&client_id="YOUR_ID_CLIENT"
 3)EXtract from the new URL the Authorization Key
 4)Put it inside the Python script
 
