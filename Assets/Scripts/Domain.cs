4de66acbad58ab77ee700848b326a8327b0154d7
Riccardo Caprile <riccardo.caprile97@gmail.com>
2024-12-17 12:33:40 +0100

    .

--- Assets/Scripts/Domain.cs
+++ Assets/Scripts/Domain.cs
@@ -67,6 +67,9 @@ public class Domain : MonoBehaviour
     {
         
         PrintAI_Thoughts();
+        Chat.counter = 0;
+        NumberPositions_Text.SetText("Number of models is : " + Chat.Number_of_Objects + "(" + (Chat.counter).ToString() + ")");
+
 
     }
 
@@ -120,9 +123,9 @@ public class Domain : MonoBehaviour
                 Generate_Script_Button.interactable = true;
                 FaultyScriptCount = 0;
                 errorcount = 0;
-             
                 Chat.CustomCoordinatesX.Clear();
                 Chat.CustomCoordinatesZ.Clear();
+
             }
 
             //------------------------------------------------- LOG FILES GENEREATION -------------------------------------------------------------------------------------------------------------------------
